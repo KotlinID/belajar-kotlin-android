1260cdcfe668c458f6feb1e961d1ce53d51aa866
Budi Oktaviyan Suryanto <budi.suryanto@go-jek.com>
2017-06-08 21:25:43 +0700

    Update testlab

--- app/src/main/kotlin/id/kotlin/app/belajarkotlin/DetailActivity.java
+++ app/src/main/kotlin/id/kotlin/app/belajarkotlin/DetailActivity.java
@@ -3,6 +3,10 @@ package id.kotlin.app.belajarkotlin;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        String text = "Click";
+        Button button = (Button) findViewById(R.id.btn_click);
+        button.setText(text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Toast.makeText(DetailActivity.this, "Halo", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextView textView = (TextView) findViewById(R.id.tv_desc);
+        textView.setText(getIntent().getStringExtra("NAMA"));
     }
 }
--- app/src/main/kotlin/id/kotlin/app/belajarkotlin/MainActivity.kt
+++ app/src/main/kotlin/id/kotlin/app/belajarkotlin/MainActivity.kt
@@ -2,11 +2,21 @@ package id.kotlin.app.belajarkotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = "Click"
+        val next = "Next"
+        btnClick.text = text
+        btnClick.setOnClickListener { toast("Halo") }
+        btnNext.text = next
+        btnNext.setOnClickListener { startActivity<DetailActivity>("NAMA" to "Budi") }
     }
 }
